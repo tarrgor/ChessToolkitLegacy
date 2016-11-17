5ced1347715a36d304e4615a0d8a27b9bf558b53
Dr. Swifty <drswifty@outlook.de>
2016-11-17 19:26:52 +0100

    Initial commit

---
+++ ChessToolkit-iOS/ChessToolkit-iOS.h
@@ -0,0 +1,19 @@
+//
+//  ChessToolkit-iOS.h
+//  ChessToolkit-iOS
+//
+//  Created by Thorsten Klusemann on 17.11.16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChessToolkit-iOS.
+FOUNDATION_EXPORT double ChessToolkit_iOSVersionNumber;
+
+//! Project version string for ChessToolkit-iOS.
+FOUNDATION_EXPORT const unsigned char ChessToolkit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChessToolkit_iOS/PublicHeader.h>
+
+
