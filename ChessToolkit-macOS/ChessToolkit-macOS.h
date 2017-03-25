c63aaeec2bf7741cf3887daa3e0ccf59b2a8d907
Thorsten Klusemann <tklusemann.dev@gmail.com>
2017-03-25 18:57:32 +0100

    Added Package.swift for SPM

---
+++ ChessToolkit-macOS/ChessToolkit-macOS.h
@@ -0,0 +1,19 @@
+//
+//  ChessToolkit-macOS.h
+//  ChessToolkit-macOS
+//
+//  Created by Thorsten Klusemann on 25.03.17.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ChessToolkit-macOS.
+FOUNDATION_EXPORT double ChessToolkit_macOSVersionNumber;
+
+//! Project version string for ChessToolkit-macOS.
+FOUNDATION_EXPORT const unsigned char ChessToolkit_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChessToolkit_macOS/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,17 @@
+//
+//  Package.swift
+//  ChessToolkit
+//
+//  Created by Thorsten Klusemann on 25.03.17.
+//
+//
+
+import PackageDescription
+
+let package = Package(
+  name: "ChessToolkit",
+  targets: [
+    Target(name: "ChessToolkit-macOS"),
+  ]
+)
+
